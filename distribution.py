68e457c11fbf0c99a61b59f4e17f30b9c4be210d
TMats <t.matsu.changmin@gmail.com>
2019-01-25 12:21:55 +0000

    change loss

--- distribution.py
+++ distribution.py
@@ -26,7 +26,7 @@ class DBlock(nn.Module):
     
 class Filtering(Normal):
     def __init__(self, b_size=50, h_size=50, z_size=8):
-        super(Filtering, self).__init__(cond_var=["b_t1"], var=["z_t1"])
+        super(Filtering, self).__init__(cond_var=["b_t1"], var=["z_t1"], name='p_b')
         self.dblock = DBlock(b_size, h_size, z_size)
         
     def forward(self,b_t1):
@@ -37,7 +37,7 @@ class Filtering(Normal):
     
 class Inference(Normal):
     def __init__(self, b_size=50, h_size=50, z_size=8):
-        super(Inference, self).__init__(cond_var=["z_t2", "b_t1", "b_t2"], var=["z_t1"])
+        super(Inference, self).__init__(cond_var=["z_t2", "b_t1", "b_t2"], var=["z_t1"], name='q')
         self.dblock = DBlock(z_size+b_size*2, h_size, z_size)
     
     def forward(self, z_t2, b_t1, b_t2):
@@ -48,7 +48,7 @@ class Inference(Normal):
 
 class Transition(Normal):
     def __init__(self, h_size=50, z_size=8):
-        super(Transition, self).__init__(cond_var=["z_t1"], var=["z_t2"])
+        super(Transition, self).__init__(cond_var=["z_t1"], var=["z_t2"], name='p_t')
         self.dblock = DBlock(z_size, h_size, z_size)
         
     def forward(self, z_t1):
@@ -59,11 +59,13 @@ class Transition(Normal):
     
 class Decoder(Bernoulli):
     def __init__(self, h_size=50, x_size=1*64*64, z_size=8):
-        super(Decoder, self).__init__(cond_var=["z_t2"], var=["x_t2"])
+        super(Decoder, self).__init__(cond_var=["z_t2"], var=["x_t2"], name='p_d')
         self.fc1 = nn.Linear(z_size, h_size)
-        self.fc2 = nn.Linear(h_size, x_size)
+        self.fc2 = nn.Linear(h_size, h_size)
+        self.fc3 = nn.Linear(h_size, x_size)
 
     def forward(self, z_t2):
         h = F.relu(self.fc1(z_t2))
-        logit = torch.sigmoid(self.fc2(h))
+        h = F.relu(self.fc2(h))
+        logit = torch.sigmoid(self.fc3(h))
         return {"probs": logit}
